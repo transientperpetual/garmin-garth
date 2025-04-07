90565951b975c91cbb4d48fee4eefb97c4c04530
transientperpetual <iamjangidankit@gmail.com>
2025-04-07 21:57:10 +0530

    understood tokens in garth session

--- auth/garmin_login.py
+++ auth/garmin_login.py
@@ -1,8 +1,9 @@
+
 import garth
 from getpass import getpass
 
-email = input("Enter email: ")
-password = getpass("Enter password: ")
+# email = input("Enter email: ")
+# password = getpass("Enter password: ")
 
 def login_to_garmin():
     email = input("Enter email: ")
@@ -12,6 +13,8 @@ def login_to_garmin():
         garth.login(email, password)
         print("Login successful!")
         # Save the session tokens
-        garth.save("~/.garth")
+        garth.save("./garth_session")
     except Exception as e:
         print(f"Login failed: {e}")
+    
+login_to_garmin()
